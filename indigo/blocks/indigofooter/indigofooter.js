1ce84a59be41bd40ecf4b41f70ed0b7932f06049
nidhigurani <108781141+nidhigurani@users.noreply.github.com>
2023-12-21 12:30:14 +0530

    carousel

---
+++ indigo/blocks/indigofooter/indigofooter.js
@@ -0,0 +1,3 @@
+var script = document.createElement('script');
+script.src = 'https://aem-dev-skyplus6e.goindigo.in/content/experience-fragments/skyplus6e/in/en/edge/footer.js?test';
+document.head.appendChild(script);
