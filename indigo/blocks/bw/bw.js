962c0ce80288ffb3d7eb046ad9f22cea3c694f58
nidhigurani <108781141+nidhigurani@users.noreply.github.com>
2023-12-21 15:05:22 +0530

    locale

--- indigo/blocks/bw/bw.js
+++ indigo/blocks/bw/bw.js
@@ -1,3 +1,18 @@
-var script = document.createElement('script');
-script.src = 'https://aem-dev-skyplus6e.goindigo.in/content/experience-fragments/skyplus6e/in/en/edge/bw.js?test';
-document.head.appendChild(script);
+//var script = document.createElement('script');
+//script.src = 'https://aem-dev-skyplus6e.goindigo.in/content/experience-fragments/skyplus6e/in/en/edge/bw.js?test';
+//document.head.appendChild(script);
+import { loadCSS, loadScript } from "../../scripts/aem.js";
+
+const widget = document.createElement("div");
+    widget.setAttribute("data-page-type", "homepage")
+    widget.setAttribute("data-component", "mf-booking-widget")
+    widget.setAttribute("data-mf-id", "mf-booking-widget")
+    widget.setAttribute("id", "booking___app")
+document.querySelector("[data-block-name='bw']").appendChild(widget);
+
+let remoteUrl = "https://app-booking-dev-skyplus6e.goindigo.in/remoteEntry.js"
+   
+const envConfig = "https://app-booking-dev-skyplus6e.goindigo.in/config/env-config.js";
+
+await loadScript(envConfig);
+await loadScript(remoteUrl);
