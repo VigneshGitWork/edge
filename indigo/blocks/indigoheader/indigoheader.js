ad4b9479c08e2d1592580bc502391867be56ac50
VigneshGitWork <153813298+VigneshGitWork@users.noreply.github.com>
2024-01-17 12:41:20 +0530

    Update indigoheader.js

--- indigo/blocks/indigoheader/indigoheader.js
+++ indigo/blocks/indigoheader/indigoheader.js
@@ -14,6 +14,8 @@ export default async function decorate(block) {
         var script = document.createElement('script');
         script.src = 'https://aem-dev-skyplus6e.goindigo.in/content/experience-fragments/skyplus6e/in/en/edge/header.js?test';
         document.head.appendChild(script);
+        // below style added to avoid the transparent header issue - style interpt with booking widget
+        document.head.insertAdjacentHTML("beforeend", `<style>#skyplus6e-header{background:#014098}</style>`)
         // },10000)
     }
 }
