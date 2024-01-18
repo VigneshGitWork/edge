05762c33c9d25e0b741910a0dee46921e22f4dbf
nidhigurani <108781141+nidhigurani@users.noreply.github.com>
2024-01-18 16:30:43 +0530

    add Skyplus wrapper

--- indigo/blocks/indigofooter/indigofooter.js
+++ indigo/blocks/indigofooter/indigofooter.js
@@ -4,4 +4,5 @@ export default async function decorate(block) {
     script.src = 'https://aem-dev-skyplus6e.goindigo.in/content/experience-fragments/skyplus6e/in/en/edge/footer.js?test';
     document.head.appendChild(script);
     // }, 9000)
+    block.classList.add("skyplus-indigo-global-wrapper-v1")
 }
