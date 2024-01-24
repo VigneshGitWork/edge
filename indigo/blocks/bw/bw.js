6fe62615bfe148d58bd6587860ea266ac705a288
nidhigurani <108781141+nidhigurani@users.noreply.github.com>
2024-01-24 15:43:03 +0530

    Update bw.js

--- indigo/blocks/bw/bw.js
+++ indigo/blocks/bw/bw.js
@@ -1,4 +1,3 @@
-
 // var script = document.createElement('script');
 // script.src = 'https://aem-dev-skyplus6e.goindigo.in/content/experience-fragments/skyplus6e/in/en/edge/bw.js?test';
 // document.head.appendChild(script);
@@ -10,14 +9,29 @@ export default async function decorate(block) {
     widget.setAttribute("data-mf-id", "mf-booking-widget")
     widget.setAttribute("data-persona", "Member")
     // widget.setAttribute("id", "booking___app")
-    document.querySelector("[data-block-name='bw']").appendChild(widget);
+    const config = {
+        mfBase: "https://app-booking-dev-skyplus6e.goindigo.in",
+    };
+    [...block.children].forEach((row, rowIndex) => {
+        let rowName;
+        [...row.children].forEach((col, colIndex) => {
+            console.log("---rrr:::", col, row)
+            if (colIndex === 0) {
+                rowName = col.textContent;
+            } else {
+ 
+                config[rowName] = col.textContent;
+            }
+        })
+    })
+    console.log("-----bw---matchd:::::", config)
+     block.innerHTML="";
+    block.appendChild(widget);
     setTimeout(async () => {
         console.log("--Timeout finished::--bw.js::::::::::::::::::::::::", document.querySelectorAll("[data-component='mf-booking-widget']"))
-
-
-        let remoteUrl = "https://app-booking-dev-skyplus6e.goindigo.in/remoteEntry.js"
-        const envConfig = "https://app-booking-dev-skyplus6e.goindigo.in/config/env-config.js";
-
+        let remoteUrl = config.mfBase + "/remoteEntry.js"
+        const envConfig = config.mfBase + "/config/env-config.js";
+ 
         await loadScript(envConfig);
         await loadScript(remoteUrl);
     })
